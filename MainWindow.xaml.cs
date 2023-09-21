9e40ebfb4141a6017653587909665853d266b7d3
Mysticmike60t <caden.finkelstein@cttech.org>
2023-09-21 18:05:50 -0400

    Stuff ig

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,25 +14,34 @@ namespace SystemDiagnostics
         private PerformanceCounter memoryCounter;
         private DispatcherTimer ramUsageTimer;
 
-        private bool isDragging = false;
-        private Point offset;
-
         public MainWindow()
         {
             InitializeComponent();
 
-            memoryCounter = new PerformanceCounter("Memory", "Available MBytes");
-            UpdateRamUsage();
+            InitializeMemoryCounter();
+            InitializeRamUsageTimer();
 
+            CenterWindowOnScreen();
             UpdateSystemInfo();
         }
 
-        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void InitializeMemoryCounter()
         {
-            if (e.ChangedButton == MouseButton.Left)
-            {
-                DragMove();
-            }
+            memoryCounter = new PerformanceCounter("Memory", "Available MBytes");
+        }
+
+        private void InitializeRamUsageTimer()
+        {
+            ramUsageTimer = new DispatcherTimer();
+            ramUsageTimer.Interval = TimeSpan.FromSeconds(1);
+            ramUsageTimer.Tick += (sender, e) => UpdateRamUsage();
+            ramUsageTimer.Start();
+        }
+
+        private void CenterWindowOnScreen()
+        {
+            Left = (SystemParameters.PrimaryScreenWidth - Width) / 2;
+            Top = (SystemParameters.PrimaryScreenHeight - Height) / 2;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -43,12 +52,6 @@ namespace SystemDiagnostics
             StartRamUsageTimer();
         }
 
-        private void UpdateRamUsage()
-        {
-            float ramUsageMB = memoryCounter.NextValue();
-            RamUsageTextBlock.Text = $"Available RAM: {ramUsageMB} MB";
-        }
-
         private void StartRamUsageTimer()
         {
             ramUsageTimer = new DispatcherTimer();
@@ -57,7 +60,22 @@ namespace SystemDiagnostics
             ramUsageTimer.Start();
         }
 
+        private void UpdateRamUsage()
+        {
+            float ramUsageMB = memoryCounter.NextValue();
+            RamUsageTextBlock.Text = $"Available RAM: {ramUsageMB} MB";
+        }
+
         private void UpdateSystemInfo()
+        {
+            UpdateCpuInfo();
+            UpdateMemoryInfo();
+            UpdateGpuInfo();
+            UpdateOsInfo();
+            UpdateDiskSpaceInfo();
+        }
+
+        private void UpdateCpuInfo()
         {
             ManagementObjectSearcher cpuSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
             foreach (ManagementObject obj in cpuSearcher.Get())
@@ -65,8 +83,10 @@ namespace SystemDiagnostics
                 string cpuInfo = $"CPU: {obj["Name"]} (Cores: {obj["NumberOfCores"]})";
                 CpuInfoTextBlock.Text = cpuInfo;
             }
+        }
 
-            // Get memory information
+        private void UpdateMemoryInfo()
+        {
             ManagementObjectSearcher memorySearcher = new ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMemory");
             ulong totalMemoryBytes = 0;
             foreach (ManagementObject obj in memorySearcher.Get())
@@ -77,7 +97,10 @@ namespace SystemDiagnostics
             double totalMemoryTB = totalMemoryGB / 1024;
             string memoryInfo = $"Total Memory: {totalMemoryGB:F2} GB";
             MemoryInfoTextBlock.Text = memoryInfo;
+        }
 
+        private void UpdateGpuInfo()
+        {
             ManagementObjectSearcher gpuSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_VideoController");
             foreach (ManagementObject obj in gpuSearcher.Get())
             {
@@ -88,48 +111,89 @@ namespace SystemDiagnostics
                 string vramInfo = $"VRAM: {FormatBytes(Convert.ToUInt64(obj["AdapterRAM"]))}";
                 GpuVramInfoTextBlock.Text = vramInfo;
             }
+        }
 
+        private void UpdateOsInfo()
+        {
             ManagementObjectSearcher osSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem");
             foreach (ManagementObject obj in osSearcher.Get())
             {
                 string osInfo = $"OS: {obj["Caption"]} (Version: {obj["Version"]})";
                 OsInfoTextBlock.Text = osInfo;
             }
+        }
 
-            // Update disk space information
-            ManagementObjectSearcher diskSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_LogicalDisk");
+        private void UpdateDiskSpaceInfo()
+        {
+            var drives = DriveInfo.GetDrives();
             var diskSpaceList = new List<DiskSpaceInfo>();
 
-            foreach (ManagementObject obj in diskSearcher.Get())
+            foreach (var drive in drives)
             {
-                if (obj["DriveType"].ToString() == "3")
+                if (drive.DriveType == DriveType.Fixed)
                 {
-                    string driveLetter = obj["Name"].ToString();
-                    ulong freeSpaceBytes = Convert.ToUInt64(obj["FreeSpace"]);
+                    string driveLetter = drive.Name;
+                    string freeSpace = $"{(double)drive.TotalFreeSpace / (1024 * 1024 * 1024):F2} GB";
 
-                    double freeSpaceGB = (double)freeSpaceBytes / (1024 * 1024 * 1024);
+                    // Calculate disk speed (read/write speed)
+                    double diskSpeed = CalculateDiskSpeed(drive);
 
-                    string freeSpace;
-                    if (freeSpaceGB >= 1024)
-                    {
-                        double freeSpaceTB = freeSpaceGB / 1024;
-                        freeSpace = $"{freeSpaceTB:F2} TB";
-                    }
-                    else
-                    {
-                        freeSpace = $"{freeSpaceGB:F2} GB";
-                    }
-                    diskSpaceList.Add(new DiskSpaceInfo { Drive = driveLetter, FreeSpace = freeSpace });
+                    string diskSpeedInfo = $"{diskSpeed:F2} MB/s";
+
+                    diskSpaceList.Add(new DiskSpaceInfo { Drive = driveLetter, FreeSpace = freeSpace, DiskSpeed = diskSpeedInfo });
                 }
             }
+
             DiskSpaceListView.ItemsSource = diskSpaceList;
         }
 
-        private class DiskSpaceInfo
+        private double CalculateDiskSpeed(DriveInfo drive)
         {
-            public string Drive { get; set; }
-            public string FreeSpace { get; set; }
+            const int bufferSize = 1024 * 1024; // 1 MB buffer size
+            byte[] buffer = new byte[bufferSize];
+            double readSpeed = 0.0, writeSpeed = 0.0;
+
+            try
+            {
+                // Measure read speed
+                using (var fileStream = new FileStream(Path.Combine(drive.RootDirectory.FullName, "read_speed_test.tmp"), FileMode.Create, FileAccess.ReadWrite, FileShare.None))
+                {
+                    Stopwatch stopwatch = Stopwatch.StartNew();
+                    while (stopwatch.ElapsedMilliseconds < 1000)
+                    {
+                        fileStream.Write(buffer, 0, buffer.Length);
+                    }
+                    stopwatch.Stop();
+                    readSpeed = (double)fileStream.Length / (stopwatch.ElapsedMilliseconds * 1000); // MB/s
+                }
+
+                // Measure write speed
+                using (var fileStream = new FileStream(Path.Combine(drive.RootDirectory.FullName, "write_speed_test.tmp"), FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+                {
+                    Stopwatch stopwatch = Stopwatch.StartNew();
+                    while (stopwatch.ElapsedMilliseconds < 1000)
+                    {
+                        fileStream.Read(buffer, 0, buffer.Length);
+                    }
+                    stopwatch.Stop();
+                    writeSpeed = (double)fileStream.Length / (stopwatch.ElapsedMilliseconds * 1000); // MB/s
+                }
+
+                // Delete test files
+                File.Delete(Path.Combine(drive.RootDirectory.FullName, "read_speed_test.tmp"));
+                File.Delete(Path.Combine(drive.RootDirectory.FullName, "write_speed_test.tmp"));
+
+                // Return the average of read and write speeds
+                return (readSpeed + writeSpeed) / 2.0;
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception here (e.g., log it or display an error message)
+                Console.WriteLine($"An error occurred while measuring disk speed: {ex.Message}");
+                return 0.0; // Return a default value or handle the error as needed
+            }
         }
+
 
         private string FormatBytes(ulong bytes)
         {
@@ -158,6 +222,7 @@ namespace SystemDiagnostics
             string memoryInfo = MemoryInfoTextBlock.Text;
             string gpuInfo = GpuInfoTextBlock.Text;
             string diskSpaceInfo = "";
+
             foreach (DiskSpaceInfo item in DiskSpaceListView.Items)
             {
                 diskSpaceInfo += $"{item.Drive}: {item.FreeSpace}\n";
@@ -211,14 +276,16 @@ namespace SystemDiagnostics
         {
             if (DiskSpaceListView.SelectedItem != null)
             {
-                // Assuming that you have a "Drive" property in your data model class
-                // Replace 'YourDataModel' with the actual class name
                 string selectedDrive = ((DiskSpaceInfo)DiskSpaceListView.SelectedItem).Drive;
-
-                // Use Process.Start to open the selected drive in File Explorer
                 Process.Start(selectedDrive);
             }
         }
 
+        private class DiskSpaceInfo
+        {
+            public string Drive { get; set; }
+            public string FreeSpace { get; set; }
+            public string DiskSpeed { get; set; } // Add this property for disk speed
+        }
     }
 }
